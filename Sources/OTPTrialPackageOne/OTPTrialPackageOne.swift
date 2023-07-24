078aa44daed50e898270629f754e4b5eae628da3
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2023-07-24 11:23:01 +0300

    edit the init

--- Sources/OTPTrialPackageOne/OTPTrialPackageOne.swift
+++ Sources/OTPTrialPackageOne/OTPTrialPackageOne.swift
@@ -1,14 +1,28 @@
 import SwiftUI
 
 public struct OTPView<Style: TextFieldStyle>: View {
-    // MARK: - Data
-    @Binding var textFieldCount: Int
-    @State internal var data: [String] = []
+    
+    private var textFieldCount: Int
+    @State private var data: [String] = []
+    private var spacing: CGFloat
+    private var alignment: VerticalAlignment
+    private var textFieldCustomStyle: Style
 
-    // MARK: - Style
-    var spacing: CGFloat
-    var alignment: VerticalAlignment
-    var textFieldCustomStyle: Style
+    public init(textFieldCount: Int, spacing: CGFloat, alignment: VerticalAlignment, textFieldCustomStyle: Style) {
+        self.textFieldCount = textFieldCount
+        self.spacing = spacing
+        self.alignment = alignment
+        self.textFieldCustomStyle = textFieldCustomStyle
+    }
+//
+//    // MARK: - Data
+//    @Binding var textFieldCount: Int
+//    @State internal var data: [String] = []
+
+//    // MARK: - Style
+//    var spacing: CGFloat
+//    var alignment: VerticalAlignment
+//    var textFieldCustomStyle: Style
 
     public var body: some View {
         HStack(alignment: alignment, spacing: spacing) {
