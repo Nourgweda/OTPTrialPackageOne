f989ac01e79c398339107268eadff52b2aaafc2f
Nourgweda <37792672+Nourgweda@users.noreply.github.com>
2023-07-24 10:27:42 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # OTPTrialPackageOne
 
 A description of this package.
+
+##Usage
+
+``` swift 
+            VerificationView(textFieldCount: .constant(4),
+                             spacing: 10, alignment: .center,
+                             textFieldCustomStyle: TextFieldStyleOne())
+```
--- Sources/OTPTrialPackageOne/OTPTrialPackageOne.swift
+++ Sources/OTPTrialPackageOne/OTPTrialPackageOne.swift
@@ -1,6 +1,23 @@
-public struct OTPTrialPackageOne {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+struct OTPView<Style: TextFieldStyle>: View {
+    // MARK: - Data
+    @Binding var textFieldCount: Int
+    @State internal var data: [String] = []
+
+    // MARK: - Style
+    var spacing: CGFloat
+    var alignment: VerticalAlignment
+    var textFieldCustomStyle: Style
+
+    var body: some View {
+        HStack(alignment: alignment, spacing: spacing) {
+            ForEach(data.indices, id: \.self) { item in
+                TextField("", text: $data[item])
+                    .textFieldStyle(textFieldCustomStyle)
+            }
+        }.onAppear {
+            data = Array(repeating: "", count: textFieldCount)
+        }
     }
 }
